ed477cf6874459251a4ec5643387ef761a9ce0bd
Méric Fournier <meric.fournier@hetic.net>
2017-12-23 22:58:41 +0100

    index

--- index.php
+++ index.php
@@ -3,3 +3,15 @@ echo 'contenu get: ';
 echo '<pre>';
 print_r($_GET);
 echo '</pre>';
+
+$bot = $_GET['bot'];
+$destinataire = $_GET['destinataire'];
+$block = $_GET['block'];
+$token = $_GET['token'];
+$attribute = $_GET['attribute'];
+$value = $_GET['value'];
+
+
+
+$query = "https://api.chatfuel.com/bots/".$bot."/users/".$destinataire."/send?chatfuel_token=".$token."&chatfuel_block_name=".$block."&".$attribute."=".$value;
+echo $query;
