55f57299bc3075fa669fdfd29d20095c775918e3
Méric Fournier <meric.fournier@hetic.net>
2017-12-23 23:18:41 +0100

    hfjfj

--- index.php
+++ index.php
@@ -15,3 +15,26 @@ $value = $_GET['value'];
 
 $query = "https://api.chatfuel.com/bots/".$bot."/users/".$destinataire."/send?chatfuel_token=".$token."&chatfuel_block_name=".$block."&".$attribute."=".$value;
 echo $query;
+
+function postBroadcast($query) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$query);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $server_output = curl_exec ($ch);
+    curl_close ($ch);
+    //if ( MODE == "preprod" || MODE == "debug" ) { $server_output = (object) array('result' => 'ok' , 'success' => true); }
+    $server_output = json_decode($server_output);
+    $result =  $server_output->result;
+    if ( $result == "ok" ) {
+      echo '{"redirect_to_blocks": ["success_contact"]}';
+    }
+    else {
+      echo '{"redirect_to_blocks": ["error_contact"]}';
+      var_dump($result);
+    }
+  }
+
+
+  postBroadcast($query);
